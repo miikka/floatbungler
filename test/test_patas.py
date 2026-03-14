f8e9fd981342258d4734df445979a1b3c18e9a2d
Miikka Koskinen <miikka.koskinen@iki.fi>
2026-03-14 12:55:18 +0200

    fix: fix patas for long inputs

--- test/test_patas.py
+++ test/test_patas.py
@@ -2,13 +2,16 @@
 #
 # SPDX-License-Identifier: ISC
 
-from hypothesis import given, strategies as st
+from floatbungler import patas
+from hypothesis import example, given
+from hypothesis import strategies as st
 from numpy.testing import assert_equal
 
-from floatbungler import patas
 
-
-@given(st.lists(st.floats(allow_nan=True)))
+@example([1.0, 2.0, 3.0] * 200)
+@example([42.0] * 1000)
+@given(st.lists(st.floats(allow_nan=True), min_size=10))
 def test_patas_encode_decode(data):
+    data = list(data)
     result = patas.decode(patas.encode(data), len(data))
     assert_equal(result, data)
